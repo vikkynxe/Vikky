050cadc45e73102eff47ddbd653ac662a5b8b22b
vikkynxe <167576760+vikkynxe@users.noreply.github.com>
2024-10-16 23:56:59 -0600

    Create my1.js

---
+++ my1.js
@@ -0,0 +1,8 @@
+// redirect.js
+function redirectToPhpPage() {
+    // Specify the PHP page URL
+    window.location.href = "your-page.php"; // Replace 'your-page.php' with the actual PHP page URL
+}
+
+// Call the function to redirect
+redirectToPhpPage();
